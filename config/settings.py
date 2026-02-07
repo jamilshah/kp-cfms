641e8f3debcc4b1bebf61483059c8fe93bf10ca6
jamilshah <shahjamil@gmail.com>
2026-02-08 00:32:01 +0500

    Add sidebar_counts context processor to templates

--- config/settings.py
+++ config/settings.py
@@ -85,6 +85,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'apps.core.context_processors.notifications',
+                'apps.core.context_processors.sidebar_counts',
             ],
         },
     },
